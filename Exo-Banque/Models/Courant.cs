02c10aed38e406f10abcee179cd8d5b4c8c960a4
Samuel Legrain <samuel.legrain@bstorm.be>
2024-02-19 11:19:42 +0100

    Ajoutez des fichiers projet.

---
+++ Exo-Banque/Models/Courant.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_Banque.Models
+{
+    internal class Courant
+    {
+        public string Numero { get; set; }
+        public Personne Titulaire { get; set; }
+
+        private double _ligneDeCredit;
+
+        public double LigneDeCredit
+        {
+            get { return _ligneDeCredit; }
+            set { 
+                if( value >= 0)
+                {
+                    _ligneDeCredit = value; 
+                }
+            }
+        }
+
+        private double _solde;
+
+        public double Solde
+        {
+            get { return _solde; }
+            private set {
+                if( value >= -LigneDeCredit)
+                {
+                    _solde = value;
+                }
+            }
+        }
+
+        public void Depot(double montant)
+        {
+            if( montant <= 0) return; // A revoir lors des Exceptions
+            Solde += montant;
+        }
+
+        public void Retrait(double montant)
+        {
+            if( montant <= 0) return; // A revoir lors des Exceptions
+            if (LigneDeCredit + Solde < montant) return; // A revoir lors des Exceptions
+            Solde -= montant;
+        }
+    }
+}
---
+++ Exo-Banque/Program.cs
@@ -0,0 +1,67 @@
+﻿using Exo_Banque.Models;
+
+namespace Exo_Banque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Personne p1 = new Personne() { 
+                Nom = "Legrain",
+                Prenom = "Samuel",
+                DateNaiss = new DateTime(1987,9,27)
+            };
+
+            Console.WriteLine($"Une personne s'appelle {p1.Nom} {p1.Prenom} et est né le {p1.DateNaiss}.");
+
+            Courant c1 = new Courant() { 
+                Numero = "BE1234",
+                LigneDeCredit = 50,
+                Titulaire = p1
+            };
+
+            string input;
+            do
+            {
+                Console.WriteLine($"Le compte {c1.Numero}, appartenant à {c1.Titulaire.Nom} {c1.Titulaire.Prenom}, a un solde de {c1.Solde} € et autorise une ligne de crédit de {c1.LigneDeCredit} €.");
+
+                Console.WriteLine("Voulez vous effectuer :");
+                Console.WriteLine("1. Dépot");
+                Console.WriteLine("2. Retrait");
+                Console.WriteLine("3. Quitter");
+                input = Console.ReadLine();
+                while (input != "1" && input != "2" && input != "3")
+                {
+                    Console.WriteLine("Erreur! Voulez vous effectuer :");
+                    Console.WriteLine("1. Dépot");
+                    Console.WriteLine("2. Retrait");
+                    Console.WriteLine("3. Quitter");
+                    input = Console.ReadLine();
+                }
+                double argent;
+                switch (input)
+                {
+                    case "1":
+                        do
+                        {
+                            Console.WriteLine("Quel montant voulez-vous déposer ?");
+                        } while (!double.TryParse(Console.ReadLine(), out argent));
+                        c1.Depot(argent);
+                        break;
+                    case "2":
+                        do
+                        {
+                            Console.WriteLine("Quel montant voulez-vous retirer ?");
+                        } while (!double.TryParse(Console.ReadLine(), out argent));
+                        c1.Retrait(argent);
+                        break;
+                    case "3":
+                        Console.WriteLine("Merci d'utiliser banque3000! Au revoir!");
+                        break;
+                } 
+            } while (input != "3");
+        }
+    }
+}
