8b197a7fa59cdefa612976ff53a071e508033a9b
Samuel Legrain <samuel.legrain@bstorm.be>
2024-02-19 17:01:42 +0100

    Ajout Correction Indexeur

---
+++ Exo-Banque/Models/Banque.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_Banque.Models
+{
+    #region Class Banque avec une List<Courant>
+    internal class Banque
+    {
+        public string Nom { get; set; }
+
+        private List<Courant> _comptes = new List<Courant>();
+
+        public Courant this[string numero]
+        {
+            get
+            {
+                foreach (Courant c in _comptes)
+                {
+                    if (c.Numero == numero) return c;
+                }
+                return null;
+            }
+        }
+
+        public void Ajouter(Courant compte)
+        {
+            if (_comptes.Contains(compte)) return; //Gestion d'Exception
+            _comptes.Add(compte);
+        }
+
+        public void Supprimer(string numero)
+        {
+            /*foreach (Courant c in _comptes)
+            {
+                if(c.Numero == numero) _comptes.Remove(c);
+            }*/
+            Courant c = this[numero];
+            if (c != null) _comptes.Remove(c);
+        }
+    }
+    #endregion
+
+    #region Class Banque avec un Dictionary<string, Courant>
+
+    //internal class Banque
+    //{
+    //    public string Nom { get; set; }
+
+    //    private Dictionary<string, Courant> _comptes = new Dictionary<string, Courant>();
+
+    //    public Courant this[string numero]
+    //    {
+    //        get
+    //        {
+    //            Courant c = null;
+    //            _comptes.TryGetValue(numero, out c);
+    //            return c;
+    //        }
+    //    }
+
+    //    public void Ajouter(Courant compte) {
+    //        if (_comptes.ContainsKey(compte.Numero)) return;
+    //        _comptes.Add(compte.Numero, compte);
+    //    }
+    //    public void Supprimer(string numero) {
+    //        _comptes.Remove(numero);
+    //    }
+    //}
+
+    #endregion
+}
--- Exo-Banque/Program.cs
+++ Exo-Banque/Program.cs
@@ -8,13 +8,33 @@ namespace Exo_Banque
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
+            Banque b1 = new Banque() { Nom = "DonneTonFric" };
+
+            Console.WriteLine($"Bienvenu chez {b1.Nom}.");
+            Console.Write("Veuillez indiquer votre nom : ");
+            string nom = Console.ReadLine();
+            Console.Write("Veuillez indiquer votre prénom : ");
+            string prenom = Console.ReadLine();
+
+            int jour, mois, annee;
+            do {
+                Console.Write("Veuillez indiquer le jour de votre naissance : ");
+            } while (!int.TryParse(Console.ReadLine(), out jour) || jour < 1 || jour > 31);
+            do {
+                Console.Write("Veuillez indiquer le mois de votre naissance : ");
+            } while (!int.TryParse(Console.ReadLine(), out mois) || mois < 1 || mois > 12);
+            do
+            {
+                Console.Write("Veuillez indiquer l'année de votre naissance : ");
+            } while (!int.TryParse(Console.ReadLine(), out annee) || annee < 1900 || annee > DateTime.Now.Year);
+
             Personne p1 = new Personne() { 
-                Nom = "Legrain",
-                Prenom = "Samuel",
-                DateNaiss = new DateTime(1987,9,27)
+                Nom = nom,
+                Prenom = prenom,
+                DateNaiss = new DateTime(annee,mois,jour)
             };
 
-            Console.WriteLine($"Une personne s'appelle {p1.Nom} {p1.Prenom} et est né le {p1.DateNaiss}.");
+            Console.WriteLine($"Bonjour {p1.Prenom} {p1.Nom}!");
 
             Courant c1 = new Courant() { 
                 Numero = "BE1234",
@@ -22,46 +42,89 @@ namespace Exo_Banque
                 Titulaire = p1
             };
 
-            string input;
+            b1.Ajouter(c1);
+
+            Console.WriteLine($"Votre premier compte est {c1.Numero}.");
+
+            string reponse;
             do
             {
-                Console.WriteLine($"Le compte {c1.Numero}, appartenant à {c1.Titulaire.Nom} {c1.Titulaire.Prenom}, a un solde de {c1.Solde} € et autorise une ligne de crédit de {c1.LigneDeCredit} €.");
-
-                Console.WriteLine("Voulez vous effectuer :");
-                Console.WriteLine("1. Dépot");
-                Console.WriteLine("2. Retrait");
-                Console.WriteLine("3. Quitter");
-                input = Console.ReadLine();
-                while (input != "1" && input != "2" && input != "3")
+                Console.WriteLine("Voulez-vous ajouter un compte ? (Oui - Non)");
+                reponse = Console.ReadLine();
+                if (reponse == "Oui")
                 {
-                    Console.WriteLine("Erreur! Voulez vous effectuer :");
+                    string numero;
+                    do
+                    {
+                        Console.WriteLine("Quel est le numéro de compte ?");
+                        numero = Console.ReadLine();
+                    } while (b1[numero] != null);
+                    double ligneCredit;
+                    do
+                    {
+                        Console.WriteLine("Quel est la limite de votre ligne de crédit ?");
+                    } while (!double.TryParse(Console.ReadLine(), out ligneCredit));
+
+                    b1.Ajouter(new Courant()
+                    {
+                        Numero = numero,
+                        LigneDeCredit = ligneCredit,
+                        Titulaire = p1
+                    });
+                }
+            } while (reponse == "Oui") ;
+
+
+
+                Courant monCompte;
+                do
+                {
+                    Console.WriteLine($"Pouvez-vous me donner votre numéro de compte ?");
+                    string numero_str = Console.ReadLine();
+                    monCompte = b1[numero_str];
+                } while (monCompte == null);
+
+
+                string input;
+                do
+                {
+                    Console.WriteLine($"Le compte {monCompte.Numero}, appartenant à {monCompte.Titulaire.Nom} {monCompte.Titulaire.Prenom}, a un solde de {monCompte.Solde} € et autorise une ligne de crédit de {monCompte.LigneDeCredit} €.");
+
+                    Console.WriteLine("Voulez vous effectuer :");
                     Console.WriteLine("1. Dépot");
                     Console.WriteLine("2. Retrait");
                     Console.WriteLine("3. Quitter");
                     input = Console.ReadLine();
-                }
-                double argent;
-                switch (input)
-                {
-                    case "1":
-                        do
-                        {
-                            Console.WriteLine("Quel montant voulez-vous déposer ?");
-                        } while (!double.TryParse(Console.ReadLine(), out argent));
-                        c1.Depot(argent);
-                        break;
-                    case "2":
-                        do
-                        {
-                            Console.WriteLine("Quel montant voulez-vous retirer ?");
-                        } while (!double.TryParse(Console.ReadLine(), out argent));
-                        c1.Retrait(argent);
-                        break;
-                    case "3":
-                        Console.WriteLine("Merci d'utiliser banque3000! Au revoir!");
-                        break;
-                } 
-            } while (input != "3");
+                    while (input != "1" && input != "2" && input != "3")
+                    {
+                        Console.WriteLine("Erreur! Voulez vous effectuer :");
+                        Console.WriteLine("1. Dépot");
+                        Console.WriteLine("2. Retrait");
+                        Console.WriteLine("3. Quitter");
+                        input = Console.ReadLine();
+                    }
+                    double argent;
+                    switch (input)
+                    {
+                        case "1":
+                            do
+                            {
+                                Console.WriteLine("Quel montant voulez-vous déposer ?");
+                            } while (!double.TryParse(Console.ReadLine(), out argent));
+                            monCompte.Depot(argent);
+                            break;
+                        case "2":
+                            do
+                            {
+                                Console.WriteLine("Quel montant voulez-vous retirer ?");
+                            } while (!double.TryParse(Console.ReadLine(), out argent));
+                            monCompte.Retrait(argent);
+                            break;
+                        case "3":
+                            Console.WriteLine($"Merci d'utiliser {b1.Nom}! Au revoir!");
+                            break;
+                    }
+                } while (input != "3");
         }
     }
 }
